5cf3ccc4642577fd7e34b3107da1560a4ac01a04
Milanjyoti Ray <milanjyotiray84@gmail.com>
2024-12-04 14:05:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"Use Strict"; // treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+let name = "Milan"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/faslse
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
